3810c8a785b5a92c9fded392458bf6874a6384eb
liang <liang.xjtu@gmail.com>
2013-08-20 15:39:28 -0500

    begin generic hidimensional addition

---
+++ hi_ge_Rtree/compile.sh
@@ -0,0 +1,1 @@
+javac -cp .:./slf4j-api-1.7.5.jar:./slf4j-simple-1.7.5.jar:./3.0.3/lib/trove-3.0.3.jar  -d . Contains.java
---
+++ hi_ge_Rtree/execute.sh
@@ -0,0 +1,1 @@
+java -cp .:./slf4j-api-1.7.5.jar:./slf4j-simple-1.7.5.jar:./3.0.3/lib/trove-3.0.3.jar net.sourceforge.jsi.examples.Contains
